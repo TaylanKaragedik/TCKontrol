53a81da5790c232856e12d180eaf695cdc85c8f9
Taylan Karagedik <45319079+TaylanKaragedik@users.noreply.github.com>
2023-08-06 18:43:17 +0300

    Proje dosyası ekle.

---
+++ TCKontrol/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TCKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "AD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SOYAD:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "YIL:";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Location = new System.Drawing.Point(148, 40);
+            this.txtTC.Name = "txtTC";
+            this.txtTC.Size = new System.Drawing.Size(168, 20);
+            this.txtTC.TabIndex = 4;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(148, 121);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(168, 20);
+            this.txtYil.TabIndex = 6;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(148, 94);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(168, 20);
+            this.txtSoyad.TabIndex = 7;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(148, 67);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(168, 20);
+            this.txtAd.TabIndex = 8;
+            this.txtAd.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(147, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 36);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SORGULA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(484, 232);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TCKontrol/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long kimlikNo = long.Parse(txtTC.Text);
+            int yil = int.Parse(txtYil.Text);
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            bool durum;
+            try
+            {
+                using (TcDogrula.KPSPublicSoapClient service = 
+                    new TcDogrula.KPSPublicSoapClient { })
+                {
+                    durum = service.TCKimlikNoDogrula(kimlikNo, ad,
+                        soyad, yil);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            if(durum == true)
+            {
+                MessageBox.Show($"{ad} {soyad} isminde birisi bulunmaktadır.");
+            }
+            else
+            {
+                MessageBox.Show("Boyle birisi yoktur");
+            }
+          
+        }
+    }
+}
