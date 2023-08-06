7c4c909dd729fb76c93b8a225943f99e6cd1356e
Taylan Karagedik <45319079+TaylanKaragedik@users.noreply.github.com>
2023-08-06 19:06:20 +0300

    Gereksiz kodlar silindi. Fonksiyon isimlendirmeleri düzeltildi.

--- TCKontrol/Form1.Designer.cs
+++ TCKontrol/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.txtYil = new System.Windows.Forms.TextBox();
             this.txtSoyad = new System.Windows.Forms.TextBox();
             this.txtAd = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.sorgulaButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -47,7 +47,6 @@
             this.label1.Size = new System.Drawing.Size(24, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "TC:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -66,7 +65,6 @@
             this.label3.Size = new System.Drawing.Size(47, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "SOYAD:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -104,18 +102,17 @@
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(168, 20);
             this.txtAd.TabIndex = 8;
-            this.txtAd.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
-            // button1
+            // sorgulaButton
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Location = new System.Drawing.Point(147, 158);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 36);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "SORGULA";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.sorgulaButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.sorgulaButton.Location = new System.Drawing.Point(147, 158);
+            this.sorgulaButton.Name = "sorgulaButton";
+            this.sorgulaButton.Size = new System.Drawing.Size(168, 36);
+            this.sorgulaButton.TabIndex = 9;
+            this.sorgulaButton.Text = "SORGULA";
+            this.sorgulaButton.UseVisualStyleBackColor = false;
+            this.sorgulaButton.Click += new System.EventHandler(this.sorgulaButtonClick);
             // 
             // Form1
             // 
@@ -123,7 +120,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(484, 232);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sorgulaButton);
             this.Controls.Add(this.txtAd);
             this.Controls.Add(this.txtSoyad);
             this.Controls.Add(this.txtYil);
@@ -150,7 +147,7 @@
         private System.Windows.Forms.TextBox txtYil;
         private System.Windows.Forms.TextBox txtSoyad;
         private System.Windows.Forms.TextBox txtAd;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sorgulaButton;
     }
 }
 
